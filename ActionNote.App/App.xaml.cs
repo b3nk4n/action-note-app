fc245c2489af70deeba9f5d328b9f92e467567dd
bsautermeister <bsautermeister@live.de>
2015-10-18 23:05:17 +0200

    Initial comming. Added framework and standard views.

---
+++ ActionNote.App/App.xaml.cs
@@ -0,0 +1,88 @@
+﻿using ActionNote.App.Views;
+using System.Threading.Tasks;
+using UWPCore.Framework.Common;
+using UWPCore.Framework.Controls;
+using UWPCore.Framework.IoC;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+
+namespace ActionNote.App
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : UniversalApp
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+            : base(typeof(MainPage), AppBackButtonBehaviour.KeepAlive, new DefaultModule())
+        {
+            InitializeComponent();
+
+            // initialize Microsoft Application Insights
+            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
+                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
+                Microsoft.ApplicationInsights.WindowsCollectors.Session);
+        }
+
+        public override Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
+        {
+            // only add the app shell when the app was not already running
+            if (args.PreviousExecutionState != ApplicationExecutionState.Running &&
+                args.PreviousExecutionState != ApplicationExecutionState.Suspended)
+            {
+                Window.Current.Content = new AppShell(
+                    RootFrame,
+                    GetNavigationMenuItems(),
+                    GetBottomDockedNavigationMenuItems());
+            }
+
+            // start the user experience
+            NavigationService.Navigate(DefaultPage);
+            return Task.FromResult<object>(null);
+        }
+
+        /// <summary>
+        /// Gets the navigation menu items.
+        /// </summary>
+        /// <returns>The navigation menu items.</returns>
+        private static NavMenuItem[] GetNavigationMenuItems()
+        {
+            return new[]
+            {
+                new NavMenuItem()
+                {
+                    Symbol = GlyphIcons.List,
+                    Label = "Notes",
+                    DestinationPage = typeof(MainPage)
+                },
+            };
+        }
+
+        /// <summary>
+        /// Gets the navigation menu items that are docked at the bottom.
+        /// </summary>
+        /// <returns>The navigation menu items.</returns>
+        private static NavMenuItem[] GetBottomDockedNavigationMenuItems()
+        {
+            return new[]
+            {
+                new NavMenuItem()
+                {
+                    Symbol = GlyphIcons.Info,
+                    Label = "About",
+                    DestinationPage = typeof(AboutPage)
+                },
+                new NavMenuItem()
+                {
+                    Symbol = GlyphIcons.Setting,
+                    Label = "Settings",
+                    DestinationPage = typeof(SettingsPage)
+                }
+            };
+        }
+    }
+}
---
+++ ActionNote.App/Views/SettingsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using UWPCore.Framework.Controls;
+
+namespace ActionNote.App.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SettingsPage : UniversalPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
