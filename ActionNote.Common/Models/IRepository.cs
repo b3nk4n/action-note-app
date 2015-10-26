56541ba74b65a62485c1f54503360a73a3a545f8
bsautermeister <bsautermeister@live.de>
2015-10-26 20:51:11 +0100

    Added repository classes.

---
+++ ActionNote.Common/Models/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace ActionNote.Common.Models
+{
+    public interface IRepository<TEntity, in TKey> where TEntity : class // TODO: move to framework?
+    {
+        void Add(TEntity entity);
+        TEntity Get(TKey id);
+        IList<TEntity> GetAll();
+        void Update(TEntity prototype);
+        void Remove(TKey id);
+        void Remove(TEntity entity);
+
+        bool Save();
+        bool Load();
+        bool HasLoaded { get; }
+
+        bool IsEmpty { get; }
+        int Count { get; }
+    }
+}
---
+++ ActionNote.Common/Models/IRepositoryItem.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ActionNote.Common.Models
+{
+    public interface IRepositoryItem<TKey> // TODO: move to framework
+    {
+        /// <summary>
+        /// Gets the Id.
+        /// </summary>
+        TKey Id { get; }
+    }
+}
