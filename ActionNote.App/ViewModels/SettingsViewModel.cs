ac1af6c41a240f71b6f263f667e5b9303cd25180
bsautermeister <bsautermeister@live.de>
2015-10-26 22:10:42 +0100

    Added settings page with view model.

---
+++ ActionNote.App/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using ActionNote.Common;
+using System;
+using UWPCore.Framework.Mvvm;
+
+namespace ActionNote.App.ViewModels
+{
+    public class SettingsViewModel : ViewModelBase
+    {
+        public bool AllowClearNotes
+        {
+            get
+            {
+                return AppSettings.AllowClearNotes.Value;
+            }
+            set
+            {
+                AppSettings.AllowClearNotes.Value = value;
+            }
+        }
+
+        public bool AllowRemoveNotes
+        {
+            get
+            {
+                return AppSettings.AllowRemoveNotes.Value;
+            }
+            set
+            {
+                AppSettings.AllowRemoveNotes.Value = value;
+            }
+        }
+    }
+}
---
+++ ActionNote.Common/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UWPCore.Framework.Storage;
+
+namespace ActionNote.Common
+{
+    public class AppSettings
+    {
+        public static StoredObjectBase<bool> AllowRemoveNotes = new LocalObject<bool>("allowRemoveNotes", true);
+
+        public static StoredObjectBase<bool> AllowClearNotes = new LocalObject<bool>("allowClearNotes", false);
+    }
+}
