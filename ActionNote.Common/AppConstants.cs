ff6bc3b463ef66e79786f458ecfe44ca981e5cad
bsautermeister <bsautermeister@live.de>
2015-11-10 15:11:46 +0100

    Redesigned main page.

--- ActionNote.Common/AppConstants.cs
+++ ActionNote.Common/AppConstants.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI;
 
 namespace ActionNote.Common
 {
@@ -20,5 +21,13 @@ namespace ActionNote.Common
         /// The ID navigation parameter.
         /// </summary>
         public const string PARAM_ID = "id=";
+
+        public readonly static Color COLOR_WHITE = Colors.White;
+        public readonly static Color COLOR_RED = Color.FromArgb(255, 255, 73, 73);
+        public readonly static Color COLOR_GREEN = Color.FromArgb(255, 97, 255, 73);
+        public readonly static Color COLOR_BLUE = Color.FromArgb(255, 73, 191, 255);
+        public readonly static Color COLOR_YELLOW = Color.FromArgb(255, 246, 255, 73);
+        public readonly static Color COLOR_VIOLETT = Color.FromArgb(255, 200, 73, 255);
+        public readonly static Color COLOR_ORANGE = Color.FromArgb(255, 255, 164, 73);
     }
 }
---
+++ ActionNote.Common/Converters/ColorCategoryToBrushConverter.cs
@@ -0,0 +1,56 @@
+﻿using ActionNote.Common.Models;
+using System;
+using Windows.UI.Xaml.Data;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace ActionNote.Common.Converters
+{
+    public class ColorCategoryToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            Color color;
+
+            if (value == null)
+                color = Colors.White;
+
+            var colorCategory = (ColorCategory)value;
+
+            switch (colorCategory)
+            {
+                case ColorCategory.Red:
+                    color = AppConstants.COLOR_RED;
+                    break;
+                case ColorCategory.Blue:
+                    color = AppConstants.COLOR_BLUE;
+                    break;
+                case ColorCategory.Green:
+                    color = AppConstants.COLOR_GREEN;
+                    break;
+                case ColorCategory.Yellow:
+                    color = AppConstants.COLOR_YELLOW;
+                    break;
+                case ColorCategory.Violett:
+                    color = AppConstants.COLOR_VIOLETT;
+                    break;
+                case ColorCategory.Orange:
+                    color = AppConstants.COLOR_ORANGE;
+                    break;
+                default:
+                    color = AppConstants.COLOR_WHITE;
+                    break;
+            }
+
+            return new SolidColorBrush(color);
+        }
+
+        /// <summary>
+        /// Not supported.
+        /// </summary>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
