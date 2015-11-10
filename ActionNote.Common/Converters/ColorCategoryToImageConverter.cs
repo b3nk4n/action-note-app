454d7143a210ce4ad5db773b0003e0701f9c3bd8
bsautermeister <bsautermeister@live.de>
2015-11-10 16:00:52 +0100

    Lightly redesigned edit page.

--- ActionNote.Common/Converters/ColorCategoryToImageConverter.cs
+++ ActionNote.Common/Converters/ColorCategoryToImageConverter.cs
@@ -1,9 +1,5 @@
 ﻿using ActionNote.Common.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.UI.Xaml.Data;
 using UWPCore.Framework.Common;
 
@@ -13,8 +9,8 @@ namespace ActionNote.Common.Converters
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            if (value == null)
-                return "/Assets/Images/neutral.png"; // error
+            if (value == null || value is NoteItem) // TODO: why was it an NOTE ITEM !?!?
+                return "/Assets/Images/neutral.png"; // error                               ==> combo-box ... scrollrad hoch ... kein icon statt neutral!!! 
 
             var color = (ColorCategory)value;
             return string.Format("/Assets/Images/{0}.png", color.ToString().FirstLetterToLower());
