a0a0bea43ff3411602c42ff89682251fde8525a1
bsautermeister <bsautermeister@live.de>
2015-12-05 20:40:40 +0100

    Localized swipe list commands

--- ActionNote.Common/Converters/FlagUnflagConverter.cs
+++ ActionNote.Common/Converters/FlagUnflagConverter.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using UWPCore.Framework.Common;
 using Windows.UI.Xaml.Data;
 
 namespace ActionNote.Common.Converters
 {
     public class FlagUnflagConverter : IValueConverter
     {
+        private static readonly Localizer _localizer = new Localizer("ActionNote.Common");
+
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return (bool)value ? "Mark as unimportant" : "Mark as important"; // TODO: translate
+            return (bool)value ? _localizer.Get("Swipe.MarkUnimportant") : _localizer.Get("Swipe.MarkImportant");
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
