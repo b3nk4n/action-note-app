1454dc5ba8a35cdf116eb62a6b1a2bafa132dcf0
bsautermeister <bsautermeister@live.de>
2015-12-30 18:46:07 +0100

    Important notes have a thicker color line.

---
+++ ActionNote.Common/Converters/ImportantCornerRadiusConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace ActionNote.Common.Converters
+{
+    public class ImportantCornerRadiusConverter : IValueConverter
+    {
+        private readonly CornerRadius IMPORTANT_RADIUS = new CornerRadius(6);
+        private readonly CornerRadius UNIMPORTANT_RADIUS = new CornerRadius(4);
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool)
+            {
+                if ((bool)value)
+                    return IMPORTANT_RADIUS;
+            }
+
+            return UNIMPORTANT_RADIUS;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ActionNote.Common/Converters/ImportantWidthConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace ActionNote.Common.Converters
+{
+    public class ImportantWidthConverter : IValueConverter
+    {
+        private const int IMPORTANT_WIDTH = 12;
+        private const int UNIMPORTANT_WIDTH = 8;
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool)
+            {
+                if ((bool)value)
+                    return IMPORTANT_WIDTH;
+            }
+
+            return UNIMPORTANT_WIDTH;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
